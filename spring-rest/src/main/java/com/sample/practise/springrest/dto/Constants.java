a14ec09fe24b22ec22ab8d504019d397e5b15804
Rambabu-Yadala <61280218+Rambabu-Yadala@users.noreply.github.com>
2020-02-21 17:26:34 +0530

    Update Constants.java

--- spring-rest/src/main/java/com/sample/practise/springrest/dto/Constants.java
+++ spring-rest/src/main/java/com/sample/practise/springrest/dto/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
 	public static final String BUY_1_GET_1 =  "BUY_1_GET_1";
 	public static final String FLAT_DISCOUNT =  "FLAT_DISCOUNT";
 	public static final String PRICE_DISCOUNT =  "PRICE_DISCOUNT";
+	public static final String URI = "http://localhost:9000/products/{id}";
 
 }
